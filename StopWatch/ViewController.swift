dcecd7c4c24060cf0ebc30617689f4be97a050d1
shivam kaushik <shivamkaushik@shivams-MacBook-Air.local>
2015-06-02 21:32:28 +0530

    best working model

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var stopwatchLabel: UILabel!
+  @IBOutlet weak var lapsTableView: UITableView!
+  @IBOutlet weak var startStopButton: UIButton!
+  @IBOutlet weak var lapResetButton: UIButton!
+  
+  var timer = NSTimer()
+  var minutes = 0
+  var seconds = 0
+  var fractions = 0
+  
+  var startStopWatch = true
+  var addLap = false
+  
+  var stopwatchString = ""
+  
+  var laps: [String] = []
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    stopwatchLabel.text = "00:00.00"
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -19,7 +38,77 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  //Tableview data source
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return laps.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+    cell.backgroundColor = self.view.backgroundColor
+    cell.textLabel?.text = "Lap \(laps.count - indexPath.row)"
+    cell.detailTextLabel?.text = laps[indexPath.row]
+    return cell
+  }
 
+  @IBAction func startStopButtonPressed(sender: UIButton) {
+    if startStopWatch == true {
+      timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateStopwatch"), userInfo: nil, repeats: true)
+      startStopWatch = false
+      
+      startStopButton.setImage(UIImage(named: "stop.png"), forState: UIControlState.Normal)
+      lapResetButton.setImage(UIImage(named: "lap.png"), forState: UIControlState.Normal)
+      addLap = true
+    } else {
+      timer.invalidate()
+      startStopWatch = true
+      startStopButton.setImage(UIImage(named: "start.png"), forState: UIControlState.Normal)
+      lapResetButton.setImage(UIImage(named: "reset.png"), forState: UIControlState.Normal)
+      addLap = false
+    }
+  }
 
+  @IBAction func lapResetButtonPressed(sender: UIButton) {
+    if addLap == true {
+      laps.insert(stopwatchString, atIndex: 0)
+      lapsTableView.reloadData()
+    } else {
+      addLap = false
+      lapResetButton.setImage(UIImage(named: "lap.png"), forState: UIControlState.Normal)
+      laps.removeAll(keepCapacity: false)
+      lapsTableView.reloadData()
+      fractions = 0
+      seconds = 0
+      minutes = 0
+      stopwatchString = "00:00.00"
+      stopwatchLabel.text = stopwatchString
+    }
+  }
+  
+  
+  func updateStopwatch() {
+    fractions += 1
+    if fractions == 100 {
+      seconds += 1
+      fractions = 0
+    }
+    
+    if seconds == 60 {
+      minutes += 1
+      seconds = 0
+    }
+    
+    let fractionString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+    let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+    let minuteString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+    
+    stopwatchString = "\(minuteString):\(secondsString).\(fractionString)"
+    stopwatchLabel.text = stopwatchString
+  }
+  
+  
+  
 }
 
